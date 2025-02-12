43df2ff1ce258426baa550305cf5b8e864e3ab8d
srsalari <sr_salari@hotmail.com>
2025-02-11 21:57:36 -0500

    Added buildspec file for CodeBuild project

--- provider.tf
+++ provider.tf
@@ -7,7 +7,14 @@ terraform {
   }
 
   required_version = ">= 1.2.0"
+
+  backend "s3" {
+    bucket = "terraform-state-file-112025" # Replace with your S3 bucket name
+    key    = "terraform.tfstate"           # State file location within the bucket
+    region = "var.region"                  # AWS region
+  }
 }
+
 
 provider "aws" {
   region = var.region
